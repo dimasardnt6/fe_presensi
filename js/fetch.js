3cfc45b029da32536a45ebca44ca170d9267d093
Dimas Ardianto <dimasardnt6@gmail.com>
2023-03-31 15:20:00 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,18 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
 let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
-    console.log(results);
+function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
