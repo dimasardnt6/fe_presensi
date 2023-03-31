031baa2c174dc233a41a57db575c7a4ade43c5ae
Dimas Ardianto <dimasardnt6@gmail.com>
2023-03-31 15:25:52 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,9 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./temp/table.js";
 let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/presensi";
-get(urlAPI,isiTablePresensi);
+get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
