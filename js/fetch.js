6a914c184aa3f9b3a02e3a3d3b09dc5a2f27c2ff
Dimas Ardianto <dimasardnt6@gmail.com>
2023-03-31 14:41:10 +0700

    push gas

---
+++ js/fetch.js
@@ -0,0 +1,6 @@
+import { get } from "https://bukulapak.github.io/api/process.js"; 
+let urlAPI = "https://dimasardnt6-ulbi.herokuapp.com/presensi";
+get(urlAPI,isiTablePresensi);
+function isiTablePresensi(results){
+    console.log(results);
+}
