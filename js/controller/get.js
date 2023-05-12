1d7409c04a6ae4d71a5eb1aa0ae53f26125cefe4
Dimas Ardianto <dimasardnt6@gmail.com>
2023-05-12 16:04:21 +0700

    push

--- js/controller/get.js
+++ js/controller/get.js
@@ -3,6 +3,15 @@ import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/
 import { isiTabel } from "../temp/tabel.js";
 export function isiTablePresensi(results) {
     results.forEach(isiRow);
+
+// Mengurutkan data dari yang terbaru
+data.sort((a, b) => new Date(b.tanggal) - new Date(a.tanggal));
+
+// Memanggil fungsi isiRow untuk setiap data yang sudah diurutkan
+for (let i = 0; i < data.length; i++) {
+    isiRow(data[i]);
+}
+
 }
 function isiRow(value) {
     let content =
@@ -12,18 +21,10 @@ function isiRow(value) {
             .replace("#LOKASI#", value.location)
             .replace("#STATUS#", value.checkin)
             .replace("#HARIKERJA#", value.biodata.hari_kerja)
-            .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
-            .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
-            .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
+            .replace("#JAMKERJA#", value.biodata.jam_kerja?value.biodata.jam_kerja[0].durasi: "#JAMKERJA#")
+            .replace("#JAMMASUK#", value.biodata.jam_kerja?value.biodata.jam_kerja[0].jam_masuk:"#JAMMASUK#")
+            .replace("#JAMKELUAR#", value.biodata.jam_kerja?value.biodata.jam_kerja[0].jam_keluar: "#JAMKELUAR#")
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner("iniTabel", content);
-}
-
-// Mengurutkan data dari yang terbaru
-data.sort((a, b) => new Date(b.tanggal) - new Date(a.tanggal));
-
-// Memanggil fungsi isiRow untuk setiap data yang sudah diurutkan
-for (let i = 0; i < data.length; i++) {
-    isiRow(data[i]);
 }
