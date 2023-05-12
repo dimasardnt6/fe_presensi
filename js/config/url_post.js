47a397f7dfd90066566ac51e9a756229cc870748
Dimas Ardianto <dimasardnt6@gmail.com>
2023-05-12 15:52:48 +0700

    push

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -2,6 +2,6 @@ export let urlPOST = "https://dimasardnt6-ulbi.herokuapp.com/ins"
 
 export function AmbilResponse(result) {
     console.log(result); //menampilkan response API pada console
-    // alert(result.message); //menampilkan response API pada alert
-    // window.location.reload(); //reload halaman setelah klik ok pada alert
+    alert(result.message); //menampilkan response API pada alert
+    window.location.reload(); //reload halaman setelah klik ok pada alert
 }
